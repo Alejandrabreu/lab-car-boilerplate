98249e39b67651a2a4ad157250f44550da24d1dc
alejandra <dubrasskabreu@gmail.com>
2017-12-06 17:08:37 -0300

    cambios carpetas

---
+++ assets/js/app.js
@@ -0,0 +1,16 @@
+// La funcionalidad de tu proyecto
+jQuery(document).ready(function(){
+  $(".oculto").hide();              
+    $(".yellow").click(function(){
+          var nodo = $(this).attr("href");  
+ 
+          if ($(nodo).is(":visible")){
+               $(nodo).hide();
+               return false;
+          }else{
+        $(".oculto").hide("slow");                             
+        $(nodo).fadeToggle("fast");
+        return false;
+          }
+    });
+}); 
